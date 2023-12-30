cd34f6da1ddfda99c7dd12954f94b37bfa091aa1
Lee-folkes <154321496+Lee-folkes@users.noreply.github.com>
2023-12-30 07:05:50 +0000

    Added background cover to mobile nav

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector('.nav');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+const backgroundCover = document.querySelector('.background-cover');
 
 navToggle.addEventListener('click',() => {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -7,8 +8,10 @@ navToggle.addEventListener('click',() => {
     if(visibility === "false") {
         primaryNav.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', true);
+        backgroundCover.setAttribute('data-visible', true);
     } else if (visibility === "true") {
         primaryNav.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
+        backgroundCover.setAttribute('data-visible', false);
     }
 });
